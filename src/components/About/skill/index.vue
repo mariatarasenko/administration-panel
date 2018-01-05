d7ea3b0051acdf4379ef193093124fb8dde69131
Maria Tarasenko <mariaatarasenko@gmail.com>
2018-01-06 02:11:18 +0300

    worked on components

---
+++ src/components/About/skill/index.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+    label.skill
+        .skill-name {{ title }}
+        .input-wrap
+            input(type="text" :value="percents" maxlenght="3").skill-input.input
+            .skill-measure %
+        .remove-btn
+            button(type="button" @click="emitRemote") -
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+        type: String,
+        default: 'Html'
+    },
+    id :{
+       type: Number 
+    },
+    percents: {
+        type: Number,
+        default: 100
+    }
+  },
+  methods: {
+      emitRemove(){
+          this.$emit('removeSkill', this.id)
+      }
+  }
+}
+</script>
+<style src="./style.scss" lang="scss" scoped></style>
+
---
+++ src/components/Works/index.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+  .about
+    h2.title Страница "Мои работы"
+    .form
+        h3.form-title Добавить работу 
+        .row
+            app-input(
+                placeholder="Название проекта"
+                type="text"
+                v-model="fields.title"
+            )
+        .row
+            app-input(
+                placeholder="Технилогии"
+                type="text"
+                v-model="fields.tech"
+            )
+        .row
+            label.upload
+                input.tipe-file(
+                    type="file"
+                    @change="getFile($event)"
+                )
+                .upload__icon
+                .upload__text Загрузить картинку
+            div.error-message {{validation.firstEror('fields.file')}}
+        .row
+            app-button(
+                title="Добавить"
+                :disabled="!fields.file || validation.hasError('fields.file') "
+                @click="sendData"
+            )
+</template>
+<script>
+import { Validator } from 'simple-vue-validator'
+import { mapActions } from 'vuex'
+
+export default {
+  mixins: [require('simple-vue-validator').mixin],
+  validators: {
+    'fields.file': (value) => {
+      return Validator.custom(() => {
+        if (Validator.isEmpty(value)) return
+
+        const allowedTypes = ['application/pdf', 'application/zip']
+
+        if (!_.includes(allowedTypes, value.type)) {
+          return 'Недопустимый формат файла, разрешены только .zip и .pdf'
+        }
+      })
+    }
+  },
+  data: () => ({
+    fields: {
+      title: '',
+      tech: '',
+      file: null
+    }
+  }),
+  methods: {
+    ...mapActions('works', ['addNewWork']),
+    getFile(event) {
+      const file = event.target.files[0]
+      this.fields.file = file
+    },
+    sendData() {
+      this.$validate().then(success => {
+        if (!success) return
+
+        const formData = new FormData()
+
+        formData.append('file', this.fields.file)
+        formData.append('tech', this.fields.tech)
+        formData.append('title', this.fields.title)
+
+        this.addNewWork(formData)
+      })
+    }
+  },
+  components: {
+    AppInput: require('_common/Input'),
+    AppButton: require('_common/Button')
+  }
+}
+</script>
+<style src="./style.scss" lang="scss" scoped></style>
+
